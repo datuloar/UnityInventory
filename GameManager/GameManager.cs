82e86804dc513a1a1eb50b55e23dc5fe15cdbb7b
Daniil Potapov <58138724+datuloar@users.noreply.github.com>
2020-11-09 01:00:24 +0500

    Corrected tabulation

--- GameManager/GameManager.cs
+++ GameManager/GameManager.cs
@@ -4,8 +4,8 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public Dictionary<GameObject, ItemComponent> ItemContainer;	
-	public ItemBase ItemDataBase;
-	[HideInInspector] public Inventory Inventory;
+    public ItemBase ItemDataBase;
+    [HideInInspector] public Inventory Inventory;
     [HideInInspector] public bool _isButtonTakeItemPressed;
     [SerializeField] private GameObject _inventoryPanel;
 
